4d11442379ddd59f57ec29ee9ab5d89a37e1edc8
Panuwat Anawatmongkhon <benzbicykle@gmail.com>
2021-04-26 09:23:30 -0700

    remove kubespray

--- KubesprayClusterOnCloudlab/experiment_coordinator/cloudlab_exp_configs/set_exps_p1.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# this file is just a placeholder at the moment. But later on, I can create a bunch of those json
-# config files and then queue up a bunch of commands to run results_retriver on them below.
--- KubesprayClusterOnCloudlab/experiment_coordinator/install_scripts/old_microservices/eshop_install.sh
+++
@@ -1,4 +0,0 @@
-bash ../install_docker_compose.sh
-git clone https://github.com/dotnet-architecture/eShopOnContainers.git
-cd ./eShopOnContainers
-sudo docker-compose up
